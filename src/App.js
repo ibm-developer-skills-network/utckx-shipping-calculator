506a338a2f6b35f4203607e93feddc6edf6c9db1
skllup7 <sapthashreek@skillup.tech>
2023-07-29 05:06:32 -0400

    changes made from theia lab environment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ShippingCalculator from './components/ShippingCalculator';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ShippingCalculator />
+    </div>
+  );
+}
+
+export default App;
