879cbe2f5a70e463ae6725636f6ad33fce5fba3b
skllup7 <100334469+skllup7@users.noreply.github.com>
2023-09-12 12:07:47 +0530

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,78 +1,47 @@
 import React, {useState} from 'react'; 
 
 const LoginForm = ({onLogin}) => { 
-
 // Initializing the state variables for username is given and complete the code for password  
-
 //using the useState hook 
-
- const [username, setUsername] = useState(''); 
-
+const [username, setUsername] = useState(''); 
 //const [] = useState(''); 
 
- // Define the handleLogin function to check the credentials 
+// Define the handleLogin function to check the credentials 
+const handleLogin = () => { 
+// Check if the username is "admin" and password is "admin" 
+// If they match, call the onLogin function to indicate successful login 
+// Otherwise, display an alert saying "Invalid username or password" 
+if (username === '' && password === '') { 
 
- const handleLogin = () => { 
 
-    // Check if the username is "admin" and password is "admin" 
+} else { 
+ 
 
-    // If they match, call the onLogin function to indicate successful login 
+} 
+}; 
 
-    // Otherwise, display an alert saying "Invalid username or password" 
+return (
+<div>
+<h2>Admin Login</h2>
+<input
+type="text"
+placeholder="Enter username"
+value={username}
+onChange={(e) => setUsername(e.target.value)}
+/>
+<input 
+type="password" 
+placeholder="Enter password" 
+//Complete the value and onChange event function 
+// value={} 
+// onChange={} 
+/> 
+{/* Add an onClick event handler to the button to call the handleLogin function */} 
+<button onClick={}>Login</button> 
 
-    // Replace the blank spaces (_____) with appropriate code 
+</div> 
 
-    if (username === '' && password === '') { 
-
-      _____; 
-
-    } else { 
-
-      _____; 
-
-    } 
-
-  }; 
-
-return ( 
-
-    <div> 
-
-      <h2>Admin Login</h2> 
-
-      <input 
-
-        type="text" 
-
-        placeholder="Enter username" 
-
-        value={username} 
-
-        onChange={(e) => setUsername(e.target.value)} 
-
-      /> 
-
-      <input 
-
-        type="password" 
-
-        placeholder="Enter password" 
-
-        //Complete the value and onChange event function 
-
-        // value={} 
-
-        // onChange={} 
-
-      /> 
-
-      {/* Add an onClick event handler to the button to call the handleLogin function */} 
-
-      <button onClick={_____}>Login</button> 
-
-    </div> 
-
-  ); 
+ ); 
 
 }; 
 
